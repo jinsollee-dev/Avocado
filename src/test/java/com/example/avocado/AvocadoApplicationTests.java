c15a2d7ea946ec405b5f9b924d4d609fc956d2a1
wongeon1 <wongeon99@naver.com>
2023-12-28 15:32:53 +0900

    Initial commit

---
+++ src/test/java/com/example/avocado/AvocadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avocado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvocadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
